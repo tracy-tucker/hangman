671aa9fedd2d7a480f1cd0d37606af52c6be6a62
Tracy Tucker <tracytuckeremail@gmail.com>
2023-10-22 18:03:54 -0500

    Adds cat to Hero comp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import cat from '../assets/cat.svg';
 
 const Hero = () => {
   return (
     <div>
       <h1>Hangman</h1>
+      <img src={cat} alt="charlie the cat" />
       <h2>Guess the Phrase</h2>
     </div>
   );
