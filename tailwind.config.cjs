294c963c47a13d69e8f01129b56f2b1e15f32427
Tracy Tucker <tracytuckeremail@gmail.com>
2023-10-24 16:22:43 -0500

    Configures theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#ebebe8',
+      green: {
+        100: '#d1e2c4',
+        300: '#778a35',
+        500: '#31352e',
+      },
+    },
     extend: {},
   },
   plugins: [],
