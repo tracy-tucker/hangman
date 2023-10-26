86d5a32ff08e2ab6970ad20b72333fded2979f19
Tracy Tucker <tracytuckeremail@gmail.com>
2023-10-26 09:46:02 -0500

    Creates alphabet box

---
+++ src/components/AlphaBox.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const AlphaBox = () => {
+  return (
+    <div className=" border-2 border-green-500 rounded-md p-3">
+      <h3 className="text-3xl text-green-300">Alphabet Box</h3>
+      {/* <div className=" flex gap-2 justify-evenly"> */}
+      <div className=" grid gap-2 grid-cols-6 justify-items-center">
+        <p className="text-2xl">A</p>
+        <p className="text-2xl">B</p>
+        <p className="text-2xl">C</p>
+        <p className="text-2xl">D</p>
+        <p className="text-2xl">E</p>
+        <p className="text-2xl">F</p>
+        <p className="text-2xl">G</p>
+        <p className="text-2xl">H</p>
+        <p className="text-2xl">I</p>
+        <p className="text-2xl">J</p>
+        <p className="text-2xl">K</p>
+        <p className="text-2xl">L</p>
+        <p className="text-2xl">M</p>
+        <p className="text-2xl">N</p>
+        <p className="text-2xl">O</p>
+        <p className="text-2xl">P</p>
+        <p className="text-2xl">Q</p>
+        <p className="text-2xl">R</p>
+        <p className="text-2xl">S</p>
+        <p className="text-2xl">T</p>
+        <p className="text-2xl">U</p>
+        <p className="text-2xl">V</p>
+        <p className="text-2xl">W</p>
+        <p className="text-2xl">Y</p>
+        <p className="text-2xl">Z</p>
+      </div>
+    </div>
+  );
+};
+
+export default AlphaBox;
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hangman from './Hangman';
+import AlphaBox from './AlphaBox';
 
 const Gameboard = () => {
   return (
     <div>
-      <Hangman />
+      <div className="align-element flex flex-col sm:flex-row justify-between items-center">
+        <Hangman />
+        <AlphaBox />
+      </div>
     </div>
   );
 };
